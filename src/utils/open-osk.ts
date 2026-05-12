8e30fcb85310073196d075f6ea8614a1104ce264
alice <aliceisjustplaying@gmail.com>
2026-05-12 21:41:26 +0000

    Convert open OSK utility to TypeScript

--- src/utils/open-osk.ts
+++ src/utils/open-osk.ts
@@ -1,16 +1,17 @@
-const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent); // https://stackoverflow.com/a/23522755
+// https://stackoverflow.com/a/23522755
+const isSafari = /^((?!chrome|android).)*safari/iu.test(navigator.userAgent);
 
-export default function openOSK() {
+export default function openOSK(): void {
   if (isSafari) {
     const fauxEl = document.createElement('input');
     fauxEl.style.position = 'absolute';
     fauxEl.style.top = '0';
     fauxEl.style.left = '0';
     fauxEl.style.opacity = '0';
-    document.body.appendChild(fauxEl);
+    document.body.append(fauxEl);
     fauxEl.focus();
     setTimeout(() => {
-      document.body.removeChild(fauxEl);
+      fauxEl.remove();
     }, 500);
   }
 }
