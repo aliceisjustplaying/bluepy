dc36d88ec642d46d65e60f8d1a598edfd2a06bd2
alice <aliceisjustplaying@gmail.com>
2026-05-20 20:59:16 +0100

    Lingui emergency fix

--- lingui.config.js
+++ lingui.config.js
@@ -3,6 +3,10 @@ import { ALL_LOCALES } from './src/locales';
 const config = {
   locales: ALL_LOCALES,
   sourceLocale: 'en',
+  formatOptions: {
+    origins: true,
+    lineNumbers: false,
+  },
   pseudoLocale: 'pseudo-LOCALE',
   fallbackLocales: {
     default: 'en',
